46599dc4d1005f24402d04b933571642b212fb46
Noyameki <dunthenerd@gmail.com>
2011-08-04 02:56:39 +0200

    Added more shells

--- shell.sql
+++ shell.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 04, 2011 at 12:14 AM
+-- Generation Time: Aug 04, 2011 at 12:55 AM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `shells` (
   `active` int(11) NOT NULL,
   `shellurl` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `shells`
@@ -40,5 +40,9 @@ CREATE TABLE IF NOT EXISTS `shells` (
 INSERT INTO `shells` (`id`, `url`, `active`, `shellurl`) VALUES
 (1, 'http://www.essopalmas.com/images/', 1, 'http://www.essopalmas.com/images/1008190708581410.jpg.php'),
 (2, 'http://www.dgmotorsports.com/images/', 1, 'http://www.dgmotorsports.com/images/saver.php'),
-(3, 'http://www.dgmotorsports.com/images/sar.php', 1, '1'),
-(4, 'http://www.worldmissphotogenic.com/images/news/', 1, 'http://www.worldmissphotogenic.com/images/news/saver.php');
+(4, 'http://www.worldmissphotogenic.com/images/news/', 1, 'http://www.worldmissphotogenic.com/images/news/saver.php'),
+(5, 'http://khamphavn.com/images/', 1, 'http://khamphavn.com/images/vodmoi.php'),
+(6, 'http://thamsan.net/images/', 1, 'http://thamsan.net/images/vodmoi.php'),
+(8, 'http://okmua.net/images/', 1, 'http://okmua.net/images/vodmoi.php'),
+(9, 'http://hanghieuthat.com/images/', 1, 'http://hanghieuthat.com/images/vodmoi.php'),
+(10, 'dongphucquangcao.com/images/', 1, 'dongphucquangcao.com/images/vodmoi.php');
